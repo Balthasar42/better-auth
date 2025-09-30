6ef6153fb3ba17c9f25b46155aef51e0a542112a
Balthasar42 <14905080+Balthasar42@users.noreply.github.com>
2025-09-30 10:40:59 +0200

    feat: add experimental authInterrupts option in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
+  experimental: {
+    authInterrupts: true,
+  },
   productionBrowserSourceMaps: process.env.SOURCE_MAPS === "true",
   typedRoutes: true,
   compiler: {
